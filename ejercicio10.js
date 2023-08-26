81b7822cb07f1a4a52c4e5be1b26720b5798d733
CassieGz <110521493+CassieGz@users.noreply.github.com>
2023-08-26 14:42:05 -0300

    Add files via upload

---
+++ ejercicio10.js
@@ -0,0 +1,31 @@
+// En ésta clase repasarás lo aprendido en clase.
+
+// Empezá creando un archivo con extensión JavaScript. Dentro definí dos variables, una llamada
+// nombre y la otra apellido y asignales un valor de tipo string.
+// Como segundo paso, hace un console.log() que dentro tenga la suma de las dos variables.
+// El resultado seguro te va a salir todo junto como NombreApellido.
+
+// Un secreto para solucionar esto, es asignarle un espacio en la suma, quedaría así:
+// console.log(nombre + “ ” + apellido).
+
+// Dentro de las comillas hay un espacio.
+
+var nombre= "Marcos";
+var apellido="Luna";
+console.log(nombre+" "+apellido);
+
+// Como segundo ejercicio, creá dos variables numéricas y luego creá una tercera variable que se
+// llame suma y dentro de ésta última, suma las primeras dos.
+// Hacé un console.log de suma.
+// Luego, creá una variable numérica más y que su contenido sea una suma entre la variable suma y un
+// número.
+// Hacé otro console.log de esa variable.
+
+var n1= 23;
+var n2=15;
+
+suma=n1+n2;
+console.log(suma);
+
+var n3= suma+2;
+console.log(n3);
